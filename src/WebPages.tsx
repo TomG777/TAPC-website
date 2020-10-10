2c98adaf1e1cbace20863d8cb3d642c67e940357
Baris Imre <barisimre98@gmail.com>
2020-10-10 14:26:15 +0200

    Add banner images

--- src/WebPages.tsx
+++ src/WebPages.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Button, Pane, Tab, TabNavigation, majorScale } from 'evergreen-ui'
+import { Button, Pane, Tab, TabNavigation, majorScale, Heading, Avatar } from 'evergreen-ui'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainPage from './Pages/MainPage';
 import InfoPage from './Pages/InfoPage';
@@ -8,19 +8,28 @@ import Scores from './Pages/Scores';
 import Rules from './Pages/Rules';
 import Participate from './Pages/Participate';
 import Contact from './Pages/Contact';
+import banner from './resources/banner.png'
+import ia from './resources/ia.png'
+
 
 export default function WebPages() {
     return (
         <BrowserRouter>
-            <Pane height={majorScale(6)} flex={0} alignItems="center" display="flex" justifyContent="center">
+            <Pane height={majorScale(6)} flex={1} alignItems="center" display="flex" justifyContent="center">
                 <TabNavigation>
                     {['Home Page', 'Information', 'Rules', 'Participate', 'Contact', 'Scores'].map((tab, index) => (
                         <Tab key={tab} is="a" href={(tab !== 'Home Page') ? tab : "/"} id={tab}>
                             {tab}
                         </Tab>
                     ))}
-
                 </TabNavigation>
+            </Pane>
+            <Pane height={majorScale(6)} flex={1} alignItems="center" display="flex" justifyContent="center">
+                <Heading is="h1" size={900}>TAPC 2020 Programming Contest</Heading>
+            </Pane>
+            <Pane  flex={1} alignItems="center" display="flex" justifyContent="center">
+                <img src={banner} style={{width: '450px', marginRight: '30px'}}/>
+                <img src={ia} style={{width: '450px', marginLeft: '30px'}}/>
             </Pane>
             <Switch>
                 <Route exact path="/">
