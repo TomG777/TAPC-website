c7172f6b8dee79acb72a96f41d006cc53e90b29d
Baris Imre <barisimre98@gmail.com>
2020-10-11 19:52:07 +0200

    Server

--- api/run.sh
+++ api/run.sh
@@ -3,4 +3,4 @@ export FLASK_ENV=development
 export FLASK_APP=app.py
 pip3 install -U flask-cors
 pip3 install Flask-Mail
-flask run --host 0.0.0.0 --port=4242
+flask run --port=4242
--- api/sandbox/app.py
+++ api/sandbox/app.py
@@ -19,11 +19,19 @@ def index():
     return 'Server Works! Maybe on the outside too?'
 
 
-@app.route('/mail')
+# def say_hello():
+#     msg = Message('Hello',
+#         sender='ICTSV Inter-Actief <tkp@inter-actief.net>',
+#         recipients=['barisimre98@gmail.com'])
+
+#     msg.body = "This is the sandbox sending"
+#     # mail.send(msg)
+#     return msg.body
+
+@app.route('/mail', methods=['POST'])
 @cross_origin()
 def say_hello():
-    msg = Message('Hello', sender='ICTSV Inter-Actief <tkp@inter-actief.net>',
-                recipients=['barisimre98@gmail.com'])
-    msg.body = "This is the sandbox sending"
-    mail.send(msg)
-    return "Sent"
+
+    first = request.form['one']
+    second = request.form['two']
+    return "ola"
