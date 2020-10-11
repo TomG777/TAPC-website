7c0beec7d3794537b0a8dfdf2977dde1ac7c34ab
Baris Imre <barisimre98@gmail.com>
2020-10-11 12:41:24 +0200

    Add server folder

---
+++ client/build/precache-manifest.5f68bcb839d3b67151291d46a5b5d609.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537ab5ede49c86d69f8d6503cfb24a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9000ab3d1c067e46ec7b",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "d0f9e68f8d03956d042d",
+    "url": "/static/js/2.63eaa1de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.63eaa1de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9000ab3d1c067e46ec7b",
+    "url": "/static/js/main.96df5d97.chunk.js"
+  },
+  {
+    "revision": "247daf366e875374c7c3",
+    "url": "/static/js/runtime-main.3a81bb70.js"
+  },
+  {
+    "revision": "dd4fed032b0389633e07ff93e6c43118",
+    "url": "/static/media/banner.dd4fed03.png"
+  },
+  {
+    "revision": "baf96011440c138d2a637a46532bbb04",
+    "url": "/static/media/ia.baf96011.png"
+  }
+]);
