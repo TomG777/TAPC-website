436d602c0d841df9b376ef3d725f6a79b2e8d181
Baris Imre <barisimre98@gmail.com>
2020-10-11 19:41:00 +0200

    Make server script better

--- api/run.sh
+++ api/run.sh
@@ -1,11 +1,6 @@
-cd sandbox
-python3 -m venv venv
-cd venv/bin
-activate
-cd ..
-cd ..
+cd ~/dev/TAPC-website/api/sandbox
 export FLASK_ENV=development
 export FLASK_APP=app.py
 pip3 install -U flask-cors
 pip3 install Flask-Mail
-flask run
+flask run --host 0.0.0.0 --port=4242
