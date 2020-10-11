28c3225ed7123c99409e88b089a9b827defdbf21
Baris Imre <barisimre98@gmail.com>
2020-10-11 12:39:59 +0200

    Minor banner change

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -14,10 +14,14 @@ export default function Banner() {
         <>
             <div style={{
                 position: 'fixed', marginLeft: 'auto', marginRight: 'auto', display: 'flex',
-                flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%', top: '0', backgroundColor: setColors.navBarBackground
+                flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                width: '100%', top: '0', backgroundColor: setColors.navBarBackground
             }}>
+                <Pane height={majorScale(6)} flex={1} alignItems="center" display="flex" justifyContent="center" marginTop={12}>
+                    <Heading color={setColors.navBarcolors} is="h1" size={900}>TAPC 2020 Programming Contest</Heading>
+                </Pane>
 
-                <Pane height={majorScale(6)} width="100%" flex={1} alignItems="center" display="flex" justifyContent="center" >
+                <Pane height={majorScale(6)} width="100%" flex={1} alignItems="center" display="flex" justifyContent="center" marginBottom={8}>
                     <TabNavigation>
                         {['Home Page', 'Information', 'Rules', 'Participate', 'Contact', 'Scores'].map((tab) => (
                             <Tab color={setColors.navBarcolors} key={tab} is="a" href={(tab !== 'Home Page') ? tab : "/"} id={tab}>
@@ -27,9 +31,7 @@ export default function Banner() {
                     </TabNavigation>
                 </Pane>
 
-                <Pane height={majorScale(6)} flex={1} alignItems="center" display="flex" justifyContent="center">
-                    <Heading color={setColors.navBarcolors} is="h1" size={900}>TAPC 2020 Programming Contest</Heading>
-                </Pane>
+
 
             </div>
             <Pane flex={1} alignItems="center" display="flex" justifyContent="center" marginTop={96}>
